a1c52bfb26cd47fff79a32c0fec3b06e93cb90a3
wbohmann <48452092+wbohmann@users.noreply.github.com>
2022-11-10 19:41:51 -0500

    Create removeDuplicates.py

---
+++ removeDuplicates.py
@@ -0,0 +1,18 @@
+#Will Bohmann
+#Week 10
+#Write and test a function called removeDuplicates(someList) that removes duplicate values from a list.
+#if we have a list of numbers, look through that list and take out duplicates and append to a new list
+
+#list of numbers
+data =[8, 17, 23, 43, 23, 8, 9, 11, 2, 17, 4, 6, 3] 
+
+def removeDuplicates(someList):       #is a name of function with a parameter
+    nodplist =[]                      #creating a variable for an empty list to hold the no duplicates
+    for elements in someList:         #to go through each element in someList parameter we make a for loop
+        if elements not in nodplist:
+            nodplist.append(elements) #add the unique elements to the nodplist variable
+    print (nodplist)
+
+removeDuplicates(data)                #the data is the argument we are passing
+
+#the function removeDuplicates will return a list called nodplist as the new list without duplicates
